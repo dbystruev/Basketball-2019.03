0f4d3d21bed763ab024a4dac49a33d927a1e1483
Denis Bystruev <dbystruev@me.com>
2019-03-25 22:40:19 +0300

    Added screen tap

---
+++ Basketball/ViewController/ViewController+@IBAction.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController+@IBAction.swift
+//  Basketball
+//
+//  Created by Denis Bystruev on 25/03/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+extension ViewController {
+    @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: sceneView)
+        let results = sceneView.hitTest(location, types: [.existingPlaneUsingExtent])
+        guard let result = results.first else { return }
+        addHoop(result: result)
+    }
+}
